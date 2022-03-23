1c68ed9e891871ae365bbc482ea5dd937bc55140
FelipeVitta <feliperiku@gmail.com>
2022-03-23 17:43:19 -0300

    projeto completo

--- CRUD.java
+++ CRUD.java
@@ -9,6 +9,8 @@ import javax.swing.event.InternalFrameEvent;
 
 public class CRUD {
 
+    //METODO PARA ADICIONAR UM NOVO CLUBE AO ARQUIVO 
+
     public void createClub(Clube a) throws IOException {
         try {
 
@@ -22,14 +24,21 @@ public class CRUD {
                 arq.seek(0);
                 arq.writeInt(0);
             }
+            //movendo o ponteiro para o inicio do arquivo
             arq.seek(0);
+            //lendo o cabeçalho
             ultimoID = arq.readInt();
+            //definindo o id do clube como o ultimo id usado + 1
             a.setIdClube(ultimoID + 1);
+            //definindo as partidas jogadas e os pontos do novo clube como 0
             a.setPontos(0);
             a.setPartidasJogadas(0);
+            //movendo o ponteiro para o inicio do arquivo
             arq.seek(0);
+            //escrevendo o id do novo clube criado (ultimo id)
             arq.writeInt(a.getIdClube());
             ba = a.toByteArray();
+            //escrevendo as informações do novo clube no arquivo
             arq.seek(arq.length());
             arq.writeChars("'");
             arq.writeInt(ba.length);
@@ -38,12 +47,15 @@ public class CRUD {
 
             arq.close();
 
+            //tratamento de erros
         } catch (Exception e) {
             System.out.println("\nERRO: Não foi possivel criar o Clube\n");
             e.printStackTrace();
         }
 
     }
+
+    //METODO QUE RETORNA AS INFORMAÇÕES DO CLUBE COM O MESMO ID INFORMADO PELO USUARIO
 
     public Clube readClub(int a) throws Exception {
 
@@ -52,26 +64,27 @@ public class CRUD {
         int tamanhoReg, ultimoID, id;
 
         RandomAccessFile arq = new RandomAccessFile("dados/clubes.db", "rw");
-        ByteBuffer buffer = ByteBuffer.allocate(4);
-        buffer.order(ByteOrder.LITTLE_ENDIAN);
         Clube c = new Clube();
+        //lendo o cabeçalho
         ultimoID = arq.readInt();
-
+         //se o id informado for menos que o ultimo id do arquivo, o clube ainda não existe
         if (a > ultimoID) {
             System.out.println("Esse Clube ainda não existe");
         } else {
 
             long tamanhoArq = arq.length();
-            
-           
+
             // enquanto não for o final do arquivo, ler o próximo
             while (arq.getFilePointer() < tamanhoArq) {
-
+                //lendo a lapide
                 lapide = arq.readChar();
                 if (lapide != '*') {
+                    //lendo o tamanho do registro e o id
                     tamanhoReg = arq.readInt();
                     id = arq.readInt();
+                    //se o id informado for igual id do arquivo
                     if (a == id) {
+                        //lendo as informações do clube e setando no objeto criado
                         c.setIdClube(id);
                         c.setNome(arq.readUTF());
                         c.setCnpj(arq.readUTF());
@@ -80,12 +93,14 @@ public class CRUD {
                         c.setPontos(arq.readInt());
                         break;
                     } else {
+                        //lendo o resto do registro
                         ba = new byte[tamanhoReg - 4];
                         arq.read(ba);
 
                     }
                 } else {
-                    tamanhoReg = arq.readInt();                                      
+                    //lendo o resto do registro
+                    tamanhoReg = arq.readInt();
                     ba = new byte[tamanhoReg];
                     arq.read(ba);
 
@@ -97,10 +112,12 @@ public class CRUD {
 
         arq.close();
 
+        //retornando o objeto criado
         return c;
 
     }
 
+    //METODO PARA DELETAR UM CLUBE (COLOCAR LAPIDE) DO ARQUIVO DE ACORDO COM O ID PASSADO
     public void deleteClub(int c) {
         try {
             byte[] ba;
@@ -108,25 +125,36 @@ public class CRUD {
             int tamanhoReg, id;
 
             RandomAccessFile arq = new RandomAccessFile("dados/clubes.db", "rw");
+            //colocando o ponteiro na posição depois do cabeçalho
             arq.seek(4);
+            //lendo o tamanho do arquivo inteiro
             long tamanhoArq = arq.length();
 
+            //enquanto não for o final do arquivo
             while (arq.getFilePointer() < tamanhoArq) {
+                //lendo a posicao atual do ponteiro
                 long pos = arq.getFilePointer();
+                //lendo a lápide
                 lapide = arq.readChar();
+                //se o arquivo não foi excluido
                 if (lapide != '*') {
+                    //lendo o tamanho do registro e o id
                     tamanhoReg = arq.readInt();
                     id = arq.readInt();
+                    //se o id do clube for igual ao id informado
                     if (c == id) {
+                        //indo para a posicao do ponteiro e colocando lapide (excluindo)
                         arq.seek(pos);
                         arq.writeChars("*");
                         break;
                     } else {
+                        //lendo o registro (volta ao loop)
                         ba = new byte[tamanhoReg - 4];
                         arq.read(ba);
                     }
 
                 } else {
+                    //lendo o registro (volta ao loop)
                     tamanhoReg = arq.readInt();
                     ba = new byte[tamanhoReg];
                     arq.read(ba);
@@ -134,6 +162,7 @@ public class CRUD {
 
             }
 
+            //tratamento de erros
         } catch (Exception e) {
             System.out.println("ERRO: Não foi possivel deletar o arquivo");
             e.printStackTrace();
@@ -141,31 +170,45 @@ public class CRUD {
 
     }
 
+    //METODO QUE RECEBE UM CLUBE E ATUALIZA SEUS VALORES DE ACORDO COM O QUE O USUÁRIO DIGITOU
+
     public void updateClub(Clube c, String nome, String cnpj, String cidade, int partidas, int pontos) {
         try {
             RandomAccessFile arq = new RandomAccessFile("dados/clubes.db", "rw");
+            //captando o tamanho do arquivo inteiro
             long tamanhoArq = arq.length();
+            //variaveis usadas no metodo
             long pos;
             char lapide;
             int tamanhoReg, id;
             byte[] bytee, ba;
+            //colocando o ponteiro do arquivo para a posição 4 (depois do cabeçalho)
             arq.seek(4);
+            //enquanto não for o final do arquivo
             while (arq.getFilePointer() < tamanhoArq) {
                 pos = arq.getFilePointer();
+                //lendo a lapide
                 lapide = arq.readChar();
                 if (lapide != '*') {
+                    //lendo o tamanho do registro e o id
                     tamanhoReg = arq.readInt();
                     id = arq.readInt();
+                    //se o valor digitado pelo usuario for igual ao id do clube
                     if (c.getIdClube() == id) {
+                        //atualizando os valores do registro
                         Clube clube = new Clube(c.getIdClube(), nome, cnpj, cidade, partidas, pontos);
                         bytee = clube.toByteArray();
+                        //se o tamanho do novo registro for menor que o tamanho do registro antigo
                         if (bytee.length < tamanhoReg) {
+                            //sobrescrevendo o novo registro no antigo
                             arq.seek(pos);
                             arq.writeChars("'");
                             arq.writeInt(tamanhoReg);
                             arq.write(bytee);
                             break;
                         } else {
+                            //colocando uma lapide no registro antigo e
+                            //indo para o final do arquivo para escrever o novo registro
                             arq.seek(pos);
                             arq.writeChars("*");
                             arq.seek(arq.length());
@@ -176,20 +219,23 @@ public class CRUD {
                             break;
                         }
                     } else {
+                        //lendo o resto do arquivo menos os bytes do id lido (voltando ao loop)
                         ba = new byte[tamanhoReg - 4];
                         arq.read(ba);
                     }
 
                 } else {
+                    //lendo o registro (voltando ao loop)
                     tamanhoReg = arq.readInt();
                     ba = new byte[tamanhoReg];
                     arq.read(ba);
                 }
 
             }
-            
+
             arq.close();
 
+        //tratando erros
         } catch (Exception e) {
             System.out.println("Não foi possivel atualizar as informações do Clube");
             e.printStackTrace();
@@ -197,25 +243,33 @@ public class CRUD {
 
     }
 
-    public void teamMatch(Clube a, int gol1, Clube b, int gol2) throws Exception{
+    //MÉTODO QUE RECEBE DOIS CLUBES E DOIS INTEIROS (GOLS) E ATUALIZA OS VALORES DE ACORDO COM O RESULTADO
 
-     RandomAccessFile arq = new RandomAccessFile("dados/clubes.db", "rw");
-     if(gol1 > gol2){
-     updateClub(a, a.getNome(), a.getCnpj(), a.getCidade(), a.getPartidasJogadas() + 1, a.getPontos() + 3);
-     updateClub(b, b.getNome(), b.getCnpj(), b.getCidade(), b.getPartidasJogadas() + 1, b.getPontos());
-     System.out.println("O "+a.getNome()+" ganhou a partida!");
-     }else if(gol1 < gol2){
-        updateClub(b, b.getNome(), b.getCnpj(), b.getCidade(), b.getPartidasJogadas() + 1, b.getPontos() + 3);
-        updateClub(a, a.getNome(), a.getCnpj(), a.getCidade(), a.getPartidasJogadas() + 1, a.getPontos());
-        System.out.println("O "+b.getNome()+" ganhou a partida!");
-     }else if(gol1 == gol2){
-        updateClub(a, a.getNome(), a.getCnpj(), a.getCidade(), a.getPartidasJogadas() + 1, a.getPontos() + 1);
-        updateClub(b, b.getNome(), b.getCnpj(), b.getCidade(), b.getPartidasJogadas() + 1, b.getPontos() + 1);
-        System.out.println("\nO jogo empatou!");
-     }
-      
+    public void teamMatch(Clube a, int gol1, Clube b, int gol2) throws Exception {
+
+        RandomAccessFile arq = new RandomAccessFile("dados/clubes.db", "rw");
+        // SE O PRIMEIRO TIME GANHAR, ADICIONAR +1 PARTIDA AOS DOIS CLUBES E ADICIONAR +
+        // 3 PONTOS PARA DO CLUBE a
+        if (gol1 > gol2) {
+            updateClub(a, a.getNome(), a.getCnpj(), a.getCidade(), a.getPartidasJogadas() + 1, a.getPontos() + 3);
+            updateClub(b, b.getNome(), b.getCnpj(), b.getCidade(), b.getPartidasJogadas() + 1, b.getPontos());
+            System.out.println("\nO " + a.getNome() + " ganhou a partida!");
+            // SE O SEGUNDO TIME GANHAR, ADICIONAR +1 PARTIDA AOS DOIS CLUBE E ADICIONAR + 3
+            // PONTOS PARA O CLUBE b
+        } else if (gol1 < gol2) {
+            updateClub(b, b.getNome(), b.getCnpj(), b.getCidade(), b.getPartidasJogadas() + 1, b.getPontos() + 3);
+            updateClub(a, a.getNome(), a.getCnpj(), a.getCidade(), a.getPartidasJogadas() + 1, a.getPontos());
+            System.out.println("\nO " + b.getNome() + " ganhou a partida!");
+            // SE A PARTIDA EMPATAR, ADICIONAR +1 PARTIDA AOS DOIS CLUBES E +1 PONTO AOS
+            // DOIS CLUBES
+        } else if (gol1 == gol2) {
+            updateClub(a, a.getNome(), a.getCnpj(), a.getCidade(), a.getPartidasJogadas() + 1, a.getPontos() + 1);
+            updateClub(b, b.getNome(), b.getCnpj(), b.getCidade(), b.getPartidasJogadas() + 1, b.getPontos() + 1);
+            System.out.println("\nO jogo empatou!");
+        }
+
+        arq.close();
+
     }
-
-    
 
 }
--- Main.java
+++ Main.java
@@ -19,6 +19,8 @@ public class Main {
       int id, pontos, partidas;
       String nome, cnpj, cidade;
 
+      //MENU DE OPÇÕES
+
       System.out.println("\n MENU \n");
       System.out.println("O que você gostaria de fazer?");
       System.out.println("\n1 - Criar um clube");
@@ -34,7 +36,7 @@ public class Main {
             // CRIAR UM CLUBE
 
             case 1:
-
+               // captando as informações do usuario
                System.out.println("\nDigite o nome do Clube\n");
                nome = sc.nextLine();
                nome = sc.nextLine();
@@ -42,6 +44,7 @@ public class Main {
                cnpj = sc.nextLine();
                System.out.println("\nDigite a cidade do Clube\n");
                cidade = sc.nextLine();
+               // passando como parametro o nome, cnpj e cidade digitados pelo usuário
                crud.createClub(new Clube(nome, cnpj, cidade));
 
                break;
@@ -49,12 +52,15 @@ public class Main {
             // LER DADOS DE UM CLUBE
 
             case 2:
+
                try {
                   RandomAccessFile arq = new RandomAccessFile("dados/clubes.db", "rw");
                   int ultimoId = arq.readInt();
+                  Clube b = new Clube();
                   System.out.println("\n");
+                  // for para mostrar todos os clubes do arquivo
                   for (int i = 1; i <= ultimoId; i++) {
-                     Clube b = crud.readClub(i);
+                     b = crud.readClub(i);
                      if (b.getIdClube() != 0) {
                         System.out.println("ID:" + b.getIdClube() + "  Nome: " + b.getNome());
                      }
@@ -62,8 +68,11 @@ public class Main {
 
                   System.out.println("\nDigite o ID do clube que deseja obter os dados: \n");
                   id = sc.nextInt();
+                  // imprimindo na tela os dados do clube com o id informado ao chamar o readClub
+                  // no CRUD
                   System.out.println(crud.readClub(id));
                   arq.close();
+                  //tratamento de erros
                } catch (Exception e) {
                   System.out.println("Não foi possivel ler o arquivo");
                   e.printStackTrace();
@@ -79,23 +88,28 @@ public class Main {
 
                   RandomAccessFile arq = new RandomAccessFile("dados/clubes.db", "rw");
                   int ultimoId = arq.readInt();
+                  Clube b = new Clube();
                   System.out.println("\n");
+                  // for para mostrar todos os clubes do arquivo
                   for (int i = 1; i <= ultimoId; i++) {
-                     Clube b = crud.readClub(i);
+                     b = crud.readClub(i);
                      if (b.getIdClube() != 0) {
                         System.out.println("ID:" + b.getIdClube() + "  Nome: " + b.getNome());
                      }
                   }
 
                   Clube clube = new Clube();
+                  // lendo o id do clube que deve ter seus dados atualizados
                   System.out.println("\nQual o ID do clube que você quer atualizar os dados?");
                   opcao = sc.nextInt();
                   System.out.println("Quer mesmo atualizar os dados desse clube?: ");
+                  // chamando o metodo no CRUD para ler os dados do clube do id informado
                   clube = crud.readClub(opcao);
                   System.out.println(clube);
                   System.out.println("\n1-Sim");
                   System.out.println("2-Não");
                   op = sc.nextInt();
+                  // confirmação de atualização de dados
                   if (op == 1) {
                      System.out.println("\nDigite o novo nome: \n");
                      nome = sc.nextLine();
@@ -104,7 +118,8 @@ public class Main {
                      cnpj = sc.nextLine();
                      System.out.println("\nDigite a cidade: \n");
                      cidade = sc.nextLine();
-
+                     // recuperando as partidas e os pontos do clube lido e passando as outras
+                     // informações para atualizar
                      partidas = clube.getPartidasJogadas();
                      pontos = clube.getPontos();
                      crud.updateClub(clube, nome, cnpj, cidade, partidas, pontos);
@@ -114,6 +129,7 @@ public class Main {
                      System.out.println("Opção inválida");
                   }
                   arq.close();
+                  // tratamento de erros
                } catch (Exception e) {
                   System.out.println("ERRO");
                   e.printStackTrace();
@@ -128,9 +144,11 @@ public class Main {
 
                   RandomAccessFile arq = new RandomAccessFile("dados/clubes.db", "rw");
                   int ultimoId = arq.readInt();
+                  Clube b = new Clube();
                   System.out.println("\n");
+                  // for para mostrar todos os clubes do arquivo
                   for (int i = 1; i <= ultimoId; i++) {
-                     Clube b = crud.readClub(i);
+                     b = crud.readClub(i);
                      if (b.getIdClube() != 0) {
                         System.out.println("ID:" + b.getIdClube() + "  Nome: " + b.getNome());
                      }
@@ -139,11 +157,13 @@ public class Main {
                   System.out.println("\nDigite o ID do clube que você quer deletar");
                   id = sc.nextInt();
                   System.out.println("Quer mesmo deletar esse Clube?: ");
+                  // chamando o metodo no CRUD para ler as informações do clube com o id informado
                   System.out.println(crud.readClub(id));
                   System.out.println("\n1-Sim");
                   System.out.println("2-Não\n");
                   op = sc.nextInt();
                   if (op == 1) {
+                     // chamando o metodo no CRUD para deletar um Clube
                      crud.deleteClub(id);
                   } else if (op == 2) {
                      System.out.println("Clube não deletado");
@@ -151,6 +171,7 @@ public class Main {
                      System.out.println("Opção inválida");
                   }
                   arq.close();
+                  // tratamento de erros
                } catch (Exception e) {
                   System.out.println("ERRO");
                   e.printStackTrace();
@@ -165,9 +186,11 @@ public class Main {
 
                   RandomAccessFile arq = new RandomAccessFile("dados/clubes.db", "rw");
                   int ultimoId = arq.readInt();
+                  Clube b = new Clube();
                   System.out.println("\n");
+                  // for para mostrar todos os clubes do arquivo
                   for (int i = 1; i <= ultimoId; i++) {
-                     Clube b = crud.readClub(i);
+                     b = crud.readClub(i);
                      if (b.getIdClube() != 0) {
                         System.out.println("ID:" + b.getIdClube() + "  Nome: " + b.getNome());
                      }
@@ -178,37 +201,48 @@ public class Main {
                   Clube clube1 = new Clube();
                   Clube clube2 = new Clube();
                   System.out.println("\nDigite o ID dos dois Clubes que gostaria de realizar a partida\n");
-
+                  // lendos os ids escritos pelo usuário
                   id1 = sc.nextInt();
                   id2 = sc.nextInt();
+
+                  // lendos as informações dos ids dos clubes escrito pelo usuario
 
                   clube1 = crud.readClub(id1);
                   clube2 = crud.readClub(id2);
 
+                  // se os dois clubes existirem no arquivo
+
                   if (clube1.getNome() != null && clube2.getNome() != null) {
+
+                     // lendo a quantidade de gols que cada clube fez
 
                      System.out.println("Quantos gols o " + clube1.getNome() + " fez?");
                      gols1 = sc.nextInt();
                      System.out.println("Quantos gols o " + clube2.getNome() + " fez?");
                      gols2 = sc.nextInt();
 
+                     // chamando o metodo para realizar partida no CRUD
                      crud.teamMatch(clube1, gols1, clube2, gols2);
+
                   } else {
                      System.out.println("Um dos Clubes não existe");
                   }
 
+                  // tratamento de erros
                } catch (Exception e) {
                   System.out.println("ERRO");
                   e.printStackTrace();
                }
                break;
 
+            // se a opcao digitada no menu for diferente das apresentadas
             default:
                System.out.println("\nValor inválido");
                break;
 
          }
 
+         // tratamento de erros
       } catch (IOException e) {
          e.printStackTrace();
       }
